8c1bb771765a86a169cbb91d86d68ed7d8ed84be
DJ Swiggett <swiggettdj@gmail.com>
2019-08-21 17:20:42 -0600

    added Ball Game

---
+++ Ball Game/Assets/controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public Rigidbody body;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            {
+                body.AddForce(transform.forward * speed);
+            }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            body.AddForce(transform.forward * -speed);
+        }
+    }
+}
