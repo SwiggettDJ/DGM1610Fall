03fee2a5f2f2503053777a6fcd6cfabf5fb00bde
DJ Swiggett <swiggettdj@gmail.com>
2019-09-30 17:10:20 -0600

    Made player move and shoot food

---
+++ 3D project/Assets/Scripts/PlayerControllerFarmer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerFarmer : MonoBehaviour
+{   public float horizontalInput;
+    public GameObject projectilePrefab;
+    private float speed = 10f;
+    private float xRange = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            horizontalInput = 0;
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            horizontalInput = 0;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 0.5f), transform.rotation);
+        }
+    }
+}
