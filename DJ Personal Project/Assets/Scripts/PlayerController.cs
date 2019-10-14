f78438434873c636836d09de394f8ba3aa74456e
DJ Swiggett <swiggettdj@gmail.com>
2019-10-14 17:16:52 -0600

    Started working on My Game

--- DJ Personal Project/Assets/Scripts/PlayerController.cs
+++ DJ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpHeight;
+    public Rigidbody body;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            body.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            body.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.transform.Rotate(new Vector3(body.transform.rotation.x, body.transform.rotation.y - 2, body.transform.rotation.z));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            body.transform.Rotate(new Vector3(body.transform.rotation.x, body.transform.rotation.y + 2, body.transform.rotation.z));
+        }
     }
 }
