24d6445fe44a56b452132d0086bdc679f1102e91
DJ Swiggett <swiggettdj@gmail.com>
2019-10-21 17:12:57 -0600

    Made background on prototype 3 scroll

--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveLeft : MonoBehaviour
+public class RepeatBackground : MonoBehaviour
 {
-    public float speed;
+    private Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if(transform.position.x < startPos.x - 50)
+        {
+            transform.position = startPos;
+        }
     }
 }
