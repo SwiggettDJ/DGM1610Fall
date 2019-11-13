48a503df9d660f58fe9c4e44adb749c6a5cf563e
DJ Swiggett <swiggettdj@gmail.com>
2019-11-13 16:55:31 -0700

    prototype 4: Finished Game

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,27 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemy;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerup;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerup, GenerateSpawnPosition(), powerup.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Powerup"));
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerup, GenerateSpawnPosition(), powerup.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -24,5 +35,12 @@ public class SpawnManager : MonoBehaviour
         float posZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(posX, 0, posZ);
         return randomPos;
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        }
     }
 }
